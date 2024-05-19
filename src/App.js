fb2bf72a4015312959caab70d14494492f9a9180
F Y Tech <fytech@Hibos-MacBook-Air.local>
2024-05-19 14:53:57 +0100

    Added book selection scroll menu, added arabic names for the books

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import './App.css';
 import PDFViewer from './PDFViewer';
 import { useState } from 'react';
 
 function App() {
-  const [previousBook, setPreviousBook] = useState('thalaathatulusuul');
-  const [currentBook, setCurrentBook] = useState('qawaacidularbac');  
-  const [nextBook, setNextBook] = useState('thalaathatulusuul');
-  const [currentBookList] = useState(['qawaacidularbac', 'thalaathatulusuul']);
+  const [previousBook, setPreviousBook] = useState('alkabaair');
+  const [currentBook, setCurrentBook] = useState('maaamaranaa');  
+  const [nextBook, setNextBook] = useState('usuulussitta');
+  const [currentBookList] = useState([
+    'maaamaranaa', 
+    'qawaacidularbac',
+    'usuulussitta',
+    'thalaathatulusuul',
+    'kitaabuttawxiid',
+    'kashf',
+    'nawaaqidh',
+    'masaailuljaahiliyyah',
+    'fadhl',
+    'usuululiimaan',
+    'alkabaair',
+  ]);
 
   const pdfs =  {
+    'maaamaranaa': {
+      'arabicName': 'واجبنا نحو ما أمرنا الله به',
+      'URL': `/maaamaranaa.pdf`,
+    },
     'qawaacidularbac': {
-      'arabicName': 'qawaacidularbac',
+      'arabicName': 'القواعد الأربع',
       'URL': `/qawaacidularbac.pdf`,
     },
+    'usuulussitta': {
+      'arabicName': 'الأصول السّتة',
+      'URL': `/usuulussitta.pdf`,
+    },    
     'thalaathatulusuul': {
-      'arabicName': 'thalaathatulusuul', 
+      'arabicName': 'الثّلاثة الأصول',
       'URL': `/thalaathatulusuul.pdf`,
+    },
+    'kitaabuttawxiid': {
+      'arabicName': 'كتاب التّوحيد',
+      'URL': `/kitaabuttawxiid.pdf`,
+    },
+    'kashf': {
+      'arabicName': 'كشف الشّبهات',
+      'URL': `/kashf.pdf`,
+    },
+    'nawaaqidh': {
+      'arabicName': 'نواقض الإسلام',
+      'URL': `/nawaaqidh.pdf`,
+    },    
+    'masaailuljaahiliyyah': {
+      'arabicName': 'مسائل الجاهليّة',
+      'URL': `/masaailuljaahiliyyah.pdf`,
+    },
+    'fadhl': {
+      'arabicName': 'فضل الإسلام',
+      'URL': `/fadhl.pdf`,
+    },
+    'usuululiimaan': {
+      'arabicName': 'أصول الإيمان',
+      'URL': `/usuululiimaan.pdf`,
+    },
+    'alkabaair': {
+      'arabicName': 'كتاب الكبائر', 
+      'URL': `/alkabaair.pdf`,
     }
   };
 
@@ -40,9 +87,9 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div style={{textAlign: 'center'}}>
       <h1>بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</h1>
-      <div>
+      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
         <p onClick={handleDecrementBook}>{pdfs[previousBook].arabicName}</p>
         <h2>{pdfs[currentBook].arabicName}</h2>
         <p onClick={handleIncrementBook}>{pdfs[nextBook].arabicName}</p>
